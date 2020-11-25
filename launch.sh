e4220130b79fb11e913a13029ba4430eae25613b
khai96_ <hvksmr1996@gmail.com>
2020-11-25 10:07:34 +0700

    refactor: Create sync_files function

--- launch.sh
+++ launch.sh
@@ -5,7 +5,11 @@ export WINEPREFIX="$HOME/.spelunky2.wine"
 cd "$(dirname "$0")"
 data_dir="$(realpath .)"
 main_dir="$(realpath ..)/MAIN"
-sync_files=(input.cfg local.cfg settings.cfg savegame.sav)
+sync_files() {
+  for name in input.cfg local.cfg settings.cfg savegame.sav; do
+    pretty-exec -- cp "$1/$name" "$2/$name"
+  done
+}
 commit_message_file=$(mktemp --suffix='.log')
 command=(
   time --format='Played for %E (%es)' --output="$commit_message_file"
@@ -13,14 +17,9 @@ command=(
   run-at "$main_dir"
   wine64 Spel2.exe
 )
-for name in "${sync_files[@]}"; do
-  pretty-exec -- cp "$data_dir/$name" "$main_dir/$name"
-done
+sync_files "$data_dir" "$main_dir"
 pretty-exec -- "${command[@]}"
-
-for name in "${sync_files[@]}"; do
-  pretty-exec -- cp "$main_dir/$name" "$data_dir/$name"
-done
+sync_files "$main_dir" "$data_dir"
 pretty-exec -- git add -v .
 pretty-exec -- git commit --allow-empty --message="$(<"$commit_message_file")"
 pretty-exec -- git push origin master
